99e85830288b35aa287fe3d450129283cc8441bc
Kimbang1 <arto135@naver.com>
2024-09-20 18:22:19 +0900

    commit

---
+++ silsp/vjs/p04/quiz02/script.js
@@ -0,0 +1,6 @@
+const dom = document.querySelector("button");
+dom.addEventListener("click",test());
+
+function test(){
+  alert("함수 호출")
+}
---
+++ silsp/vjs/p05/script/script.js
@@ -0,0 +1,16 @@
+console.log("숫자   누적값");
+console.log("-------------");
+let x = 1;
+let sum = 0;
+sum += x;
+console.log(x + "       " + sum);
+sum += ++x;
+console.log(x + "       " + sum);
+sum += ++x;
+console.log(x + "       " + sum);
+sum += ++x;
+console.log(x + "       " + sum);
+sum += ++x;
+console.log(x + "       " + sum);
+
+console.log("\nPrint OK!");
